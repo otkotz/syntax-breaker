46d550a049fdc153f4ec0ea7413e0b8a23182283
kamillo <kamil.kocot@wp.pl>
2026-05-19 01:35:13 +0200

    feat: add HUD with HP bar, gold counter, and stage label

---
+++ scripts/ui/hud.gd
@@ -0,0 +1,32 @@
+class_name HUD
+extends Control
+
+@onready var hp_bar: ProgressBar = $TopBar/HPBar
+@onready var gold_label: Label = $TopBar/GoldLabel
+@onready var stage_label: Label = $TopBar/StageLabel
+@onready var cooldown_container: HBoxContainer = $BottomBar/CooldownContainer
+
+var _player: Player
+
+func setup(player: Player) -> void:
+	_player = player
+	_player.hp_changed.connect(_on_hp_changed)
+	GameBus.gold_changed.connect(_on_gold_changed)
+	_update_stage()
+
+func _on_hp_changed(current: float, maximum: float) -> void:
+	hp_bar.max_value = maximum
+	hp_bar.value = current
+
+func _on_gold_changed(amount: int) -> void:
+	gold_label.text = str(amount)
+
+func _update_stage() -> void:
+	stage_label.text = "Stage %d" % RunManager.current_stage
+
+func update_cooldowns(skill_instances: Array[SkillInstance], timers: Array[float]) -> void:
+	for i in mini(skill_instances.size(), cooldown_container.get_child_count()):
+		var bar: ProgressBar = cooldown_container.get_child(i) as ProgressBar
+		if bar:
+			bar.max_value = skill_instances[i].computed_stats.get("cooldown", 1.0)
+			bar.value = max(timers[i], 0.0)
